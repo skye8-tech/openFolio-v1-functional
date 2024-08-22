87873ccf9570d9bfb676de50164394635a1eb47d
Snr Andy <nfonandrew73@gmail.com>
2024-08-22 14:05:35 +0100

    fix register from @kewirjewel

--- sql/db.sql
+++ sql/db.sql
@@ -1,56 +1,53 @@
-CREATE DATABASE OPENFOLIO();
-
-
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    username TEXT UNIQUE NOT NULL,
-    passwords VARCHAR(50) NOT NULL,
-    email TEXT NOT NULL,
-    address TEXT NOT NULL,
-    profileDetails TEXT(100) NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE certifications (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name TEXT UNIQUE NOT NULL,
-    Title TEXT(30) NOT NULL,
-    organisation TEXT(40) NOT NULL,
-    description TEXT(250) NULL,
-    image VARCHAR NULL,
-    url VARCHAR NULL,
-    issuer TEXT NULL,
-    dateEarned date NULL,
-    user_id INTEGER NOT NULL REFERENCES users  
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    Title VARCHAR(30) NOT NULL,
+    organisation VARCHAR(40) NOT NULL,
+    description VARCHAR(250) NULL,
+    image VARCHAR(255) NULL,
+    url VARCHAR(255) NULL,
+    issuer VARCHAR(100) NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-
 CREATE TABLE projects (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name TEXT UNIQUE NOT NULL,
-    Title TEXT(30) NOT NULL,
-    description TEXT(250) NULL,
-    TechnologiesUsed TEXT(30) NOT NULL,
-    projectLinks VARCHAR NOT NULL,
-    image VARCHAR NULL,
-    url VARCHAR NULL,
-    user_id INTEGER NOT NULL REFERENCES users
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    Title VARCHAR(30) NOT NULL,
+    description VARCHAR(250) NULL,
+    TechnologiesUsed VARCHAR(100) NOT NULL,
+    projectLinks VARCHAR(255) NOT NULL,
+    image VARCHAR(255) NULL,
+    url VARCHAR(255) NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE skills (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name TEXT UNIQUE NOT NULL,
-    proficiencyLevel TEXT(20) NOT NULL,
-    description TEXT(250) NULL,
-    user_id INTEGER NOT NULL REFERENCES users
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    proficiencyLevel VARCHAR(20) NOT NULL,
+    description VARCHAR(250) NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE profile (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(100) NULL,
     last_name VARCHAR(100) NULL,
     bio TEXT NULL,
-    image TEXT NULL,
+    image VARCHAR(255) NULL,
     phone VARCHAR(32) NULL,
-    user_id INTEGER NOT NULL REFERENCES users
-
-)
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
