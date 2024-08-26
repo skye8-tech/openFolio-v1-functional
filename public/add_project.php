1f217b63c55e56b325444c906c1a34aba5e11b6a
RandyTAR <br.randytar@gmail.com>
2024-08-26 09:45:18 +0100

    created viewing project page

--- public/add_project.php
+++ public/add_project.php
@@ -16,7 +16,7 @@ if(isset($_SESSION['message'])){?>
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="projectUploads/css/bootstrap.min.css"> -->
 </head>
 <body>
     <div class="container my-5 bg-lightpurple justify-content-center col-md-6 offset-md-3 shadow-md mt-5 p-5 rounded-5">
@@ -73,6 +73,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             ?>
             <script>alert("New project created successfully");</script>
             <?php 
+            header("location:viewing_projects.php");
             } else {
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             }
--- public/fetching_users.php
+++ public/fetching_users.php
@@ -4,7 +4,7 @@
 include "../config/config.php";
     ?>
 <?php
-$sql = "SELECT id, username, email, full_name, address, bio, contact FROM users where id=2";
+$sql = "SELECT id, username, email, full_name, address, bio, contact FROM users where id";
 $result= mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 ?>
@@ -16,7 +16,7 @@ $row = mysqli_fetch_assoc($result);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="projectUploads/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="style.css">
 </head>
 <body class="bg-light">
@@ -31,7 +31,7 @@ $row = mysqli_fetch_assoc($result);
                             <div class="container">
                             <img
                             class="img rounded-circle img-fluid"
-                            src="./defualt.png" alt=""></div>
+                            src="projectUploads/.png" onerror="this.src='projectUploads/defualt.png'" alt=""></div>
                             <h2 class="font-weight-bold mt-4"><?php echo $row['username']?></h2>
                             <p><?php echo $row['email']?></p>
                             <p><?php echo $row['contact']?></p>
