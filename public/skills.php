1f38e9123ea792fb9f05bdc48fd05fa7eae02825
mbabidblessing <mbabidb@gmail.com>
2024-08-29 05:38:46 +0100

    fetched skills from database

--- public/skills.php
+++ public/skills.php
@@ -39,21 +39,28 @@
 </div>
 <?php
 include '../config/config.php';
+session_start();
 
-if ($conn -> connect_error) {
-  echo "Failed to connect to MySQL: " . $conn -> connect_error;
-  exit();
-}
+//$user_id =$_SESSION['user_id'];
 
-// Perform query
-if ($result = $conn -> query("SELECT * FROM Skills")) {
-     echo "Returned rows are: " . $result -> num_rows;
-  // Free result set
-  $result = $conn -> store_result();
-  
+  if($conn -> connect_error){
+    die ('connection failed:' . $conn->connect_error);
+  }
+//fetching data fron the database
+$query = "SELECT * FROM skills where user_id = 1";
+$result =mysqli_query($conn, $query);
+if(mysqli_num_rows($result)>0){
+    while($row = mysqli_fetch_assoc($result)){
+ echo $row['name'];
+ echo $row['proficiencyLevel'];
+ echo $row['description'];
+ 
+   }
+}else{
+echo "No data skill found on the skill table";
 }
-$conn -> close();
-   
+$conn->close();
+  // }
 ?>
 
 </body>
